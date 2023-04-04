edb1fb0a86d217218c5a33aca91b18be65595d28
RollsRoycewk <50765560@qq.com>
2023-04-04 22:22:25 +0800

    fix: 解决控制台报错

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,10 @@
 const { defineConfig } = require('@vue/cli-service')
+
+const MonacaWebpackPlugin = require('monaco-editor-webpack-plugin')
+
 module.exports = defineConfig({
+  configureWebpack: {
+    plugins: [new MonacaWebpackPlugin()],
+  },
   transpileDependencies: true,
 })
