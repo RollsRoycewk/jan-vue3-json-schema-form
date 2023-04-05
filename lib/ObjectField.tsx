4fb76044d671f1b165dd0e7187a5bf9af019a23f
RollsRoycewk <50765560@qq.com>
2023-04-05 20:47:08 +0800

    feat: 完成ObjectField的渲染

--- lib/ObjectField.tsx
+++ lib/ObjectField.tsx
@@ -1,17 +1,49 @@
 import { defineComponent, inject } from 'vue'
 import { FiledPropsDefine } from './types'
 import { SchemaFormContextKey } from './context'
+import { isObject } from './utils'
+
+// const TypeHelperComponent = defineComponent({
+//   props: FiledPropsDefine,
+// })
+
+// type SchemaItemDefine = typeof TypeHelperComponent
 
 export default defineComponent({
   name: 'ObjectField',
   props: FiledPropsDefine,
-  setup() {
-    const context: any = inject(SchemaFormContextKey)
-
-    console.log('context=========>', context)
-
+  setup(props) {
     return () => {
-      return <div>Object Field</div>
+      // SchemaItem组件 父级结点provide分发过来的
+      const context: any = inject(SchemaFormContextKey)
+
+      const handleObjectFieldChange = (k: string, v: any) => {
+        const value: any = isObject(props.value) ? props.value : {}
+
+        if (v === undefined) {
+          delete value[k]
+        } else {
+          value[k] = v
+        }
+
+        props.onChange(value)
+      }
+
+      const { schema, rootSchema, value } = props
+
+      const { SchemaItem } = context
+      const properties = schema.properties || {}
+      const currentValue: any = isObject(value) ? value : {}
+
+      return Object.keys(properties).map((k: string, index: number) => (
+        <SchemaItem
+          schema={properties[k]}
+          rootSchema={rootSchema}
+          value={currentValue[k]}
+          key={index}
+          onChange={(v: any) => handleObjectFieldChange(k, v)}
+        />
+      ))
     }
   },
 })
