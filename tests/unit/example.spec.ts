7e5c73898ca488656d4a7dc7e3e301284277ea7e
RollsRoycewk <50765560@qq.com>
2023-04-06 14:09:36 +0800

    feat: jest测试初体验

--- tests/unit/example.spec.ts
+++ tests/unit/example.spec.ts
@@ -1,12 +1,77 @@
-// import { shallowMount } from '@vue/test-utils'
-// import HelloWorld from '@/components/HelloWorld.vue'
+import { shallowMount } from '@vue/test-utils'
+import { defineComponent, h, resolveComponent } from 'vue'
 
-// describe('HelloWorld.vue', () => {
-//   it('renders props.msg when passed', () => {
-//     const msg = 'new message'
-//     const wrapper = shallowMount(HelloWorld, {
-//       props: { msg },
-//     })
-//     expect(wrapper.text()).toMatch(msg)
-//   })
-// })
+const HelloWorld = defineComponent({
+  name: 'HelloWorld',
+  props: {
+    msg: String,
+  },
+  setup(props) {
+    return () => {
+      return h('div', props.msg)
+    }
+  },
+})
+
+describe('HelloWorld.vue', () => {
+  beforeEach(() => {
+    console.log('before each')
+  })
+
+  afterEach(() => {
+    console.log('after each')
+  })
+
+  beforeAll(() => {
+    console.log('before all')
+  })
+
+  afterAll(() => {
+    console.log('after all')
+  })
+
+  it('renders props.msg when passed', async () => {
+    const msg = 'new message'
+    const wrapper = shallowMount(HelloWorld, {
+      props: { msg },
+    })
+
+    // await wrapper.setProps({
+    //   msg: '123',
+    // })
+
+    // expect(wrapper.text()).toEqual('123') 通过
+
+    wrapper.setProps({
+      msg: '123',
+    })
+
+    expect(wrapper.text()).toEqual('123')
+  })
+
+  it('should work', () => {
+    expect(1 + 1).toBe(2)
+  })
+})
+
+describe('another', () => {
+  beforeEach(() => {
+    console.log('before each11111')
+  })
+
+  afterEach(() => {
+    console.log('after each1111')
+  })
+
+  beforeAll(() => {
+    console.log('before all1111')
+  })
+
+  afterAll(() => {
+    console.log('after all111')
+  })
+
+  it('should work', () => {
+    expect(1 + 1).toBe(2)
+  })
+})
