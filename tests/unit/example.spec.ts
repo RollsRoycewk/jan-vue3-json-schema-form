a61ca1176c7ca9e8e1eab9af230f91ebd5b94248
RollsRoycewk <50765560@qq.com>
2023-04-06 22:27:45 +0800

    feat: 单元测试补全

--- tests/unit/example.spec.ts
+++ tests/unit/example.spec.ts
@@ -18,5 +18,11 @@ describe('JsonSchemaForm', () => {
 
     const numberField = wrapper.findComponent(NumberField)
     expect(numberField.exists()).toBeTruthy()
+    // await numberField.props('onChange')('123')  适用于第三方组件,不需要关心底层是怎么实现的
+    // 这里input是我们自己写的 所以需要测试
+    const input = numberField.find('input')
+    input.element.value = '123'
+    input.trigger('input')
+    expect(value).toBe(123)
   })
 })
