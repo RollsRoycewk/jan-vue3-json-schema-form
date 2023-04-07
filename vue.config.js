5e32d3d833e56686639da33be91e253559902b7f
RollsRoycewk <50765560@qq.com>
2023-04-07 09:47:14 +0800

    feat: 拆分主题的代码打包

--- vue.config.js
+++ vue.config.js
@@ -2,9 +2,11 @@ const { defineConfig } = require('@vue/cli-service')
 
 const MonacaWebpackPlugin = require('monaco-editor-webpack-plugin')
 
+const isLib = process.env.TYPE === 'lib'
+
 module.exports = defineConfig({
   configureWebpack: {
-    plugins: [new MonacaWebpackPlugin()],
+    plugins: isLib ? [] : [new MonacaWebpackPlugin()],
   },
   transpileDependencies: true,
 })
